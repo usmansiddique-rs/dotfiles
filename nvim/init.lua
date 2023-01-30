f0342f9064ff30e99d8e20c833a8cf48e9d76db9
usman1515 <usmansiddique1515@gmail.com>
2023-01-30 19:07:26 +0500

    created nvim main config

---
+++ nvim/init.lua
@@ -0,0 +1,15 @@
+-- import files within the core folder
+
+-- parent folder
+require("usman.plugins-setup")
+
+-- core folder
+require("usman.core.colorschemes")
+require("usman.core.keymaps")
+require("usman.core.options")
+
+-- plugins folder
+require("usman.plugins.comment")
+require("usman.plugins.nvim-tree")
+require("usman.plugins.lualine")
+
