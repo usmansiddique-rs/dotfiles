9d79af3f1db349fb050a457045c3c10b5afa6ca9
usman1515 <usmansiddique1515@gmail.com>
2023-02-24 15:07:13 +0500

    updated nvim plugin lualine config

--- nvim/lua/usman/plugins/lualine.lua
+++ nvim/lua/usman/plugins/lualine.lua
@@ -5,17 +5,54 @@ if not status then
 end
 
 -- get lualine 16color theme
-local lualine_16color = require("lualine.themes.16color")
+local theme_16color = require("lualine.themes.16color")
 -- get lualine dracula theme
-local lualine_dracula = require("lualine.themes.dracula")
+local theme_dracula = require("lualine.themes.dracula")
 -- get lualine nightfly theme
-local lualine_nightfly = require("lualine.themes.nightfly")
+local theme_nightfly = require("lualine.themes.nightfly")
 -- get lualine powerline theme
-local lualine_powerline = require("lualine.themes.powerline")
+local theme_powerline = require("lualine.themes.powerline")
+-- get lualine powerline_dark theme
+local theme_powerline_dark = require("lualine.themes.powerline_dark")
 
 -- configure lualine with modified theme
-lualine.setup({
+lualine.setup{
     options = {
-        theme = lualine_powerline
+        icons_enabled = true,
+        theme = theme_powerline_dark,
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        },
     },
-})
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {{'filename', file_status = true, path = 0}},
+        lualine_c = {'branch', 'diff', 'diagnostics'},
+        lualine_x = {
+            {'diagnostics', sources = { "nvim_diagnostic" }, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '}},
+            'encoding',
+            'fileformat',
+            'filetype'
+        },
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    },
+    inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { {
+            'filename',
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+        }},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+    },
+}
