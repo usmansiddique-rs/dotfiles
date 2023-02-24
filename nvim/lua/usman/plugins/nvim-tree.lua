4074a1b8d1b1b8bbfc9300e708bde082a0654e22
usman1515 <usmansiddique1515@gmail.com>
2023-02-24 15:48:08 +0500

    updated nvim plugin nvim-tree config file

--- nvim/lua/usman/plugins/nvim-tree.lua
+++ nvim/lua/usman/plugins/nvim-tree.lua
@@ -4,6 +4,10 @@ if not setup then
     return
 end
 
+-- disable netrw (strongly advised). for nvim-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- recommended settings from nvim-tree documentation
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
